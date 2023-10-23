c18db565c8c01d6488a93bf9e7d7f62dab96c877
Adam Novotný <adamelmo@seznam.cz>
2023-10-23 06:40:13 +0200

    added math problems

---
+++ src/components/MathProblem.tsx
@@ -0,0 +1,27 @@
+// MathProblem.js
+import React from 'react';
+
+const MathProblem = ({ problem, choices, onSelectAnswer }) => {
+  return (
+    <div>
+      <p>{problem.question}</p>
+      <ul>
+        {problem.choices.map((choice, index) => (
+          <li key={index}>
+            <label>
+              <input
+                type="radio"
+                name="answer"
+                value={choice}
+                onChange={() => onSelectAnswer(choice, problem.correctAnswer)}
+              />
+              {choice}
+            </label>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MathProblem;
